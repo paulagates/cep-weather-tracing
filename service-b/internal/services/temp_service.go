dfa2929ab2dc3b15e3d2f0327b74a838503d6e5c
PAULA CAROLINE NUNES DA SILVA <paulac@tce.mt.gov.br>
2025-04-02 15:56:46 -0400

    Adiciona query scape

--- service-b/internal/services/temp_service.go
+++ service-b/internal/services/temp_service.go
@@ -2,10 +2,10 @@ package services
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -21,17 +21,13 @@ type WeatherResponse struct {
 
 func GetTemperatureFromCity(city string) (WeatherResponse, error) {
 	apiKey := os.Getenv("WEATHER_API_KEY")
-	url := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=%s", apiKey, city)
+
+	url := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=%s", apiKey, url.QueryEscape(city))
 	resp, err := http.Get(url)
 	if err != nil {
 		return WeatherResponse{}, err
 	}
 	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return WeatherResponse{}, errors.New("failed to fetch weather data")
-	}
-
 	var weather WeatherResponse
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
